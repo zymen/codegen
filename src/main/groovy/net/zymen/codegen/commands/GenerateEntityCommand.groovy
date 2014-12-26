0757d82f031c4cf54685aa03b031768488db3f81
Boguslaw Faja <boguslaw.faja@allegro.pl>
2014-12-27 00:24:07 +0100

    Upgrading template for entity, adding code for generating content of Entity

--- src/main/groovy/net/zymen/codegen/commands/GenerateEntityCommand.groovy
+++ src/main/groovy/net/zymen/codegen/commands/GenerateEntityCommand.groovy
@@ -1,8 +1,10 @@
 package net.zymen.codegen.commands
 
 import net.zymen.codegen.Ioc
+import net.zymen.codegen.factory.TemplateFactory
 import net.zymen.codegen.model.Entity
 import net.zymen.codegen.service.DirFileService
+import net.zymen.codegen.service.OutputBuilderService
 
 class GenerateEntityCommand implements Command{
     DirFileService dirFileService
@@ -25,5 +27,10 @@ class GenerateEntityCommand implements Command{
     def execute() {
         String destinationDirectory = "src/main/groovy/${entity.pack}/model"
         this.dirFileService.createDirectory(destinationDirectory)
+
+        OutputBuilderService outputBuilderService = new OutputBuilderService()
+        String output = outputBuilderService.output(TemplateFactory.fromFile("entity.template"), this.entity.properties)
+
+        new File(destinationDirectory + "/" + this.entity.name + ".java").write(output)
     }
 }
