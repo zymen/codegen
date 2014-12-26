efb299887b0f3995774ba5ec2f526baea62497d6
Boguslaw Faja <boguslaw.faja@allegro.pl>
2014-12-26 23:41:49 +0100

    Adding constructors that gives it possible to write tests for GenerateEntityCommand

--- src/main/groovy/net/zymen/codegen/commands/GenerateEntityCommand.groovy
+++ src/main/groovy/net/zymen/codegen/commands/GenerateEntityCommand.groovy
@@ -1,16 +1,29 @@
 package net.zymen.codegen.commands
 
+import net.zymen.codegen.Ioc
 import net.zymen.codegen.model.Entity
+import net.zymen.codegen.service.DirFileService
 
 class GenerateEntityCommand implements Command{
+    DirFileService dirFileService
+
     Entity entity
 
     public GenerateEntityCommand(Entity entity) {
+        this(
+                entity,
+                Ioc.instance().get(DirFileService.class)
+        )
+    }
+
+    public GenerateEntityCommand(Entity entity, DirFileService dirFileService) {
         this.entity = entity
+        this.dirFileService = dirFileService
     }
 
     @Override
     def execute() {
-        return null
+        String destinationDirectory = "src/main/groovy/${entity.package}/model/${entity.name}"
+        println "creating directory if required..."
     }
 }
